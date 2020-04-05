6b5d0735aa96724c697f49ffc5cac53198c6b9c1
Gustaf Lindholm <babiluskus@gmail.com>
2020-04-05 17:21:28 +0200

    better jwt handling. alertifyJs for errors and messages, bootswatch for theming

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,32 +1,36 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {}
 
   register() {
     this.authService.register(this.model).subscribe(
       () => {
-        console.log('registration successfull');
+        this.alertify.success('registration successfull');
       },
-      error => {
-        console.log(error);
+      (error) => {
+        this.alertify.error(error);
       }
     );
   }
 
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('cancel');
+    this.alertify.message('Registration canceled');
   }
 }
