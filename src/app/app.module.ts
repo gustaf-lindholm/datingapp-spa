21783cfbedd0fc988ae7bfccaad43c63c67a6832
Gustaf Lindholm <babiluskus@gmail.com>
2020-04-11 13:52:30 +0200

    service, resolver and component for updating user. CanDeactivate route guard on edit user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,9 @@ import { MemberCardComponent } from './members/member-list/member-card/member-ca
 import { MemberDetailComponent } from './members/member-list/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-list/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guards';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -50,6 +53,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
     MessagesComponent,
     MemberCardComponent,
     MemberDetailComponent,
+    MemberEditComponent,
   ],
   imports: [
     BrowserModule,
@@ -71,8 +75,10 @@ export class CustomHammerConfig extends HammerGestureConfig {
   providers: [
     ErrorInterceptorProvider,
     AuthService,
+    PreventUnsavedChanges,
     MemberDetailResolver,
-    MemberListResolver
+    MemberListResolver,
+    MemberEditResolver,
   ],
   bootstrap: [AppComponent],
 })
